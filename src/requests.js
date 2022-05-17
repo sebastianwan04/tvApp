2e3ff3e5758f8cdb48283d3c78a2a16a54e4eff8
sebastian.wan04 <sebastian.wan04@gmail.com>
2022-05-17 11:57:58 +0200

    Set up requests

---
+++ src/requests.js
@@ -0,0 +1,10 @@
+export const getShowsByKey = (key) => {
+    return fetch(`https://api.tvmaze.com/search/shows?q=${key}`)
+        .then(resp => resp.json())
+}
+
+
+export const getShowsById = (id) => {
+    return fetch(`https://api.tvmaze.com/shows/${id}?embed=cast`)
+        .then(resp => resp.json())
+}
